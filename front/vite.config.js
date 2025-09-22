02da8b331212ba1376dc610299c9c12f21402e7b
ungraduated <85732427+ungraduated@users.noreply.github.com>
2025-09-22 10:53:41 +0900

    test4

--- front/vite.config.js
+++ front/vite.config.js
@@ -8,12 +8,12 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/rental': {
-        target: 'http://mocaapp.net/',
+        target: 'http://mocaapp.net',
         changeOrigin: true,
         secure: false,
       },
       '/api': {
-        target: 'http://mocaapp.net/',
+        target: 'http://mocaapp.net',
         changeOrigin: true,
         secure: false,
       }
